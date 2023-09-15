a771d8058c8693edba056e3c3b50fa304b82d53b
alve06001 <alve06001@E-PW0234LA>
2023-09-15 13:40:12 +0200

    Add project files.

---
+++ Uppgift 2.4 inl/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är person 1 s lön?");
+            int pers1 =int.Parse(Console.ReadLine());
+            Console.WriteLine("Vad är person 2 s lön?");
+            int pers2= int.Parse(Console.ReadLine());
+            Console.WriteLine("Vad är person 3 s lön?");
+            int pers3= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("medel lönen är " + ((pers1 + pers2 + pers3)/3) + " mellan de 3 anställda");
+
+        }
+    }
+}
